5ee771fd87f5d3ee9052628737374fea6b3bafad
Abheek Ghosh <abheekghosh.ag@gmail.com>
2016-08-10 21:55:25 +0530

    game able to beat candidates

---
+++ play.py
@@ -0,0 +1,54 @@
+from timeit import default_timer as timer
+import numpy as np
+import matplotlib.pyplot as plt
+
+from state import State
+from printer import gamePrinter
+
+# Size of Grid
+# rows = int(input('Enter number of dots rows: '))
+# cols = int(input('Enter number of dots columns: '))
+# if(rows < 1 | cols < 1):
+# 	print('Please enter valid grid.')
+# else:
+# 	State.setGrid(rows, cols, False)
+
+State.setGrid(5,5, False)
+
+# Depth, here for each level of depth ony one player moves
+time_lim = 0.1
+depth = 3
+
+# define start state
+play = State(0, 0, 0, np.zeros(State.nEdges))
+game = np.zeros(State.nEdges)
+
+# You start the game
+maxi = False
+while(play.nTotal < State.nEdges):
+	start = timer()
+	if(maxi):
+		move = play.next_move(maxi, int(depth))
+	else:
+		move = int(input('Please enter your move: '))
+
+	game[play.nTotal] = move
+	maxi = maxi ^ (play.add(move, maxi))
+	# temp = abs((State.nEdges - play.nTotal)/(State.nEdges - play.nTotal - depth))
+	# if(temp > 1):
+	#depth = depth * 1.035
+
+	gamePrinter(x.Played, x.Score, State.rDots, State.cDots)
+	print('The move was: ', move)
+	print('Depth = ', depth)
+
+	end = timer()
+	if(end - start < time_lim):
+		depth = depth + 1
+
+if(play.result() == 1):
+	print('Sorry. You lost.')
+elif(play.result() == 0.5):
+	print('Draw. Just little more effort.')
+else:
+	print('Congratulations. You won.')
--- state.py
+++ state.py
@@ -23,7 +23,7 @@ class State:
 
 	# Change the game details
 	@classmethod
-	def setGrid(cls, r, c, newData):
+	def setGrid(cls, r=4, c=4, newData=False):
 		cls.rDots = r
 		cls.cDots = c
 		cls.nEdges = cls.rDots * (cls.cDots - 1) + cls.cDots * (cls.rDots - 1)
@@ -58,26 +58,30 @@ class State:
 	@classmethod
 	def compute(cls, param):
 		z1 = State.Theta1.dot(param)
-		z1 = cls.sigmoid(z1)
+		z1 = np.concatenate(([1], cls.sigmoid(z1)))
 		z1 = State.Theta2.dot(z1)
 		z1 = cls.sigmoid(z1)
 		return z1
 
 	#Initialization
-	def __init__(self, nTotal, pAg, pOpp, Played):
+	def __init__(self, nTotal=0, pAg=0, pOpp=0, Played=[]):
 		self.reset(nTotal, pAg, pOpp, Played)
 
-	def reset(self, nTotal=0, pAg=0, pOpp=0, Played=np.zeros(nEdges)):
+	def reset(self, nTotal=0, pAg=0, pOpp=0, Played=[]):
 		self.nTotal = nTotal
 		self.pAg = pAg
 		self.pOpp = pOpp
 		self.Played = Played
+		self.Score = np.zeros((State.rDots - 1) * (State.cDots - 1))
 
 	def add(self, edge, maxi):
+		if(self.Played[edge] == 1):
+			print('invalid move')
+			return False
 		self.nTotal = self.nTotal + 1
 		self.Played[edge] = 1
 
-		point = self.isPoint(edge)
+		point = self.isPoint(edge, maxi)
 		if(point):
 			if(maxi):
 				self.pAg = self.pAg + point
@@ -92,7 +96,7 @@ class State:
 		else:
 			return 0
 
-	def isPoint(self, edge):
+	def isPoint(self, edge, maxi):
 		x = 0
 		y = 0
 		phase = State.rDots * (State.cDots - 1)
@@ -101,12 +105,16 @@ class State:
 				x = self.allTrue(edge, edge + (State.cDots-1), 
 					phase + int(edge/(State.cDots-1))*(State.cDots) + int(edge % (State.cDots-1)),
 					phase + int(edge/(State.cDots-1))*(State.cDots) + int(edge % (State.cDots-1)) + 1)
+				if(x == 1):
+					self.Score[edge] = maxi
 
 			if(edge >= State.cDots - 1):
 				edge = edge - (State.cDots-1)
 				y = self.allTrue(edge, edge + (State.cDots-1), 
 					phase + int(edge/(State.cDots-1))*(State.cDots) + int(edge % (State.cDots-1)),
 					phase + int(edge/(State.cDots-1))*(State.cDots) + int(edge % (State.cDots-1)) + 1)
+				if(y == 1):
+					self.Score[edge] = maxi
 
 		else:
 			e = edge - phase
@@ -114,6 +122,8 @@ class State:
 				x = self.allTrue(edge, edge + 1, 
 					int(e/State.cDots)*(State.cDots-1) + e % State.cDots,
 					(int(e/State.cDots)+1)*(State.cDots-1) + e % State.cDots)
+				if(y == 1):
+					self.Score[int(e/State.cDots)*(State.cDots-1) + e % State.cDots] = maxi
 
 			if(e % State.cDots != 0):
 				edge = edge - 1
@@ -121,29 +131,71 @@ class State:
 				y = self.allTrue(edge, edge + 1, 
 					int(e/State.cDots)*(State.cDots-1) + e % State.cDots,
 					(int(e/State.cDots)+1)*(State.cDots-1) + e % State.cDots)
+				if(y == 1):
+					self.Score[int(e/State.cDots)*(State.cDots-1) + e % State.cDots] = maxi
 
 		return (x + y)
 
 
 	def result(self):
 		if(self.pAg > self.pOpp):
-			return 100
+			return 1
 		elif(self.pAg == self.pOpp):
-			return 0
+			return 0.5
 		else:
-			return -100
+			return 0
 
 	def value(self, maxi, d):
-		if(self.nTotal == self.nEdges):
+		val = 0 if maxi else 1
+		if(d == 0):# or (maxi and d == 1)):
+			param = np.concatenate(([1], self.Played, [self.pAg], [self.pOpp], [maxi]))
+			val = State.compute(param)
+		elif(self.nTotal == State.nEdges):
 			return self.result()
-		elif(d == 0):
-			param = np.concatenate(([1], self.Played.copy(), [self.pAg], [self.pOpp], [maxi]))
-			return State.compute(param)
 		else:
-			pass
+			x = State(self.nTotal, self.pAg, self.pOpp, self.Played.copy())
+			for i in range(State.nEdges):
+				if(x.Played[i] == 1):
+					continue
+				else:
+					temp_maxi = maxi ^ x.add(i, maxi)
+					#print('State =',self.nTotal,'Depth =',d,',Value =',val,', Next move =',i,", Maxi =",maxi)
+					if(maxi):
+						val = max(val, x.value(temp_maxi, d-1))
+					else:
+						val = min(val, x.value(temp_maxi, d-1))
+					
+					x.Played[i] = 0
+					x.pAg = self.pAg
+					x.pOpp = self.pOpp
+					x.nTotal = self.nTotal
+		
+		#print(val)
+		return val
 
 	def next_move(self, maxi, d):
-		if(d == 0):
-			print("Some problem.")
-		else:
-			pass
+		x = State(self.nTotal, self.pAg, self.pOpp, self.Played.copy())
+		move = -1
+		max_value = -1
+		for i in range(State.nEdges):
+			#print('Checking...', i)
+			if(x.Played[i] == 1):
+				continue
+			else:
+				temp_maxi = maxi ^ x.add(i, maxi)
+				val = x.value(temp_maxi, d-1)
+				if(val > max_value):
+					#print('State =',self.nTotal,'Depth =',d,',Value =',val,', Next move =',i,", Maxi =",maxi)
+					move = i
+					max_value = val
+				
+				x.Played[i] = 0
+				x.pAg = self.pAg
+				x.pOpp = self.pOpp
+				x.nTotal = self.nTotal
+
+		#print('Move = ', move, ", Max value =", max_value)
+		if(move < 0 or max_value < 0):
+			print('Error. No move.')
+
+		return move	
